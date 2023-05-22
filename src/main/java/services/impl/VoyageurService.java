6618d1ea868df81a7137258df7c9fd2a2cb78503
LKerdraon <lydiekerdraon@gmail.com>
2023-05-22 16:50:55 +0200

    Ajout Voyageur

---
+++ src/main/java/services/impl/VoyageurService.java
@@ -0,0 +1,42 @@
+package services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.inti.entities.Voyageur;
+import com.inti.repositories.VoyageurRepository;
+
+import services.interfaces.IVoyageurService;
+
+public class VoyageurService implements IVoyageurService{
+
+	@Autowired
+	VoyageurRepository voyageurRepository;
+	
+	
+	@Override
+	public List<Voyageur> findAll() {
+		// TODO Auto-generated method stub
+		return voyageurRepository.findAll();
+	}
+
+	@Override
+	public Voyageur findOne(Long id) {
+		// TODO Auto-generated method stub
+		return voyageurRepository.findById(id).get();
+	}
+
+	@Override
+	public Voyageur save(Voyageur voyageur) {
+		// TODO Auto-generated method stub
+		return voyageurRepository.save(voyageur);
+	}
+
+	@Override
+	public void delete(Long id) {
+		voyageurRepository.deleteById(id);
+		
+	}
+
+}
